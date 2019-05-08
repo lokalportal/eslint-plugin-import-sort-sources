f555843ac8dea058dab0c1a60a368ffce8cdbbfc
André Sattler <a.sattler@lokalportal.de>
2019-05-08 14:30:24 +0200

    replaced boilerplate tests wit actual tests

--- tests/lib/rules/import-order.js
+++ tests/lib/rules/import-order.js
@@ -1,18 +1,31 @@
 const rule = require('../../../lib/rules/import-order')
 const RuleTester = require('eslint').RuleTester
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015, sourceType: 'module' } });
 ruleTester.run('import-order', rule, {
   valid: [
-    'var validExample;',
-    'for(;;) {}',
-    'do {} while(condition)'
+    `
+    import x from 'a'
+    import s from 'b'
+    import { something } from 'c'
+    `,  
+    `
+    import x from 'a'
+    import { something, more } from 'c'
+    import { somethingElse } from 'c'
+    `  
   ],
 
   invalid: [
     {
-      code: 'while(condition) {}',
-      errors: [ { messageId: 'avoidWhileLoops' } ]
+      code: 
+        `
+        import x from 'c'
+        import { something, more } from 'b'
+        import { somethingElse } from 'c'
+        `  
+      ,
+      errors: [ { message: 'sort your imports!' } ]
     }
   ]
 });
